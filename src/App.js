306dea2bdc78916c872fd548e444a0b61f6b9cdb
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-03 18:39:45 +0300

    addition to subscribing

--- src/App.js
+++ src/App.js
@@ -24,25 +24,13 @@ class Application extends Component {
     this.unsubscribe();
   }
 
+  // т.к. мы подписались на изменения в database, теперь мы просто отправляем запросы add/delete, и состояние обновляется само
   handleCreate = async (post) => {
-    const { posts } = this.state;
-
-    const docRef = await firestore.collection("posts").add(post); // создаст новый post в firestore
-    const doc = await docRef.get(); // теперь нужно обновить состояние приложения, мы можем просто запрашивать каждый раз список всех постов, тоесть не только в componentDidMount, но и после handleCreate, однако мы можем сделать лучше, после содания поста, нам сразу возвращается ссылка на него, соответственно так мы можем получить наш только что созданный post
-
-    const newPost = collectIdsAndDocs(doc);
-
-    this.setState({ posts: [newPost, ...posts] }); // и потом добавить его в state
+    await firestore.collection("posts").add(post);
   };
 
   handleRemove = async (id) => {
-    const { posts: allPosts } = this.state; // переименовали чтобы проще было сетстейтить
-
     await firestore.doc(`posts/${id}`).delete();
-
-    const posts = allPosts.filter((post) => post.id !== id);
-
-    this.setState({ posts });
   };
 
   render() {
