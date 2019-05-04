c0220138ab78b90f1170f8db2963e1fb9790baa0
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-04 16:23:40 +0300

    added signUp with email and password

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 
+import { auth } from "../firebase";
+
 class SignUp extends Component {
   state = { displayName: "", email: "", password: "" };
 
@@ -9,8 +11,19 @@ class SignUp extends Component {
     this.setState({ [name]: value });
   };
 
-  handleSubmit = (event) => {
+  handleSubmit = async (event) => {
     event.preventDefault();
+
+    const { displayName, email, password } = this.state;
+
+    if (!displayName || !email || !password) return;
+
+    try {
+      const { user } = await auth.createUserWithEmailAndPassword(email, password);
+      user.updateProfile({ displayName });
+    } catch (error) {
+      console.log(error);
+    }
 
     this.setState({ displayName: "", email: "", password: "" });
   };
