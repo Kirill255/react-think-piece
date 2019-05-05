51947be22b2612aeeefe4c248ea2b33b9573db1a
Kirill255 <24504648+Kirill255@users.noreply.github.com>
2019-05-05 10:52:24 +0300

    changing UserProfile displayName

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,8 +1,50 @@
 import React, { Component } from "react";
 
+import { auth, firestore } from "../firebase";
+
 class UserProfile extends Component {
+  state = { displayName: "" };
+
+  get uid() {
+    return auth.currentUser.uid;
+  }
+
+  get userRef() {
+    return firestore.doc(`users/${this.uid}`);
+  }
+
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const { displayName } = this.state;
+
+    if (displayName) {
+      this.userRef.update({ displayName });
+    }
+  };
+
   render() {
-    return <div>I am the user profile page.</div>;
+    const { displayName } = this.state;
+
+    return (
+      <section className="UserProfile">
+        <form onSubmit={this.handleSubmit} className="UpdateUser">
+          <input
+            type="text"
+            name="displayName"
+            value={displayName}
+            placeholder="Display Name"
+            onChange={this.handleChange}
+          />
+          <input className="update" type="submit" value="Update" />
+        </form>
+      </section>
+    );
   }
 }
 
